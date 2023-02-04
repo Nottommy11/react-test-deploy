170ae6f8bdfa46562fc38034575b1ca327146957
Nottommy11 <thomas.marxsen11@gmail.com>
2023-02-04 16:21:40 -0600

    WHY

--- src/index.js
+++ src/index.js
@@ -34,32 +34,18 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/contact", element: <Contact /> },
-      { path: "/flashcards", element: <Flashcards /> },
-      { path: "/games", element: <Games /> },
-      { path: "/quizzes", element: <Quizzes /> },
+      { path: "/react-test-deploy/", element: <Home /> },
+      { path: "/react-test-deploy/", element: <About /> },
+      { path: "/react-test-deploy/", element: <Contact /> },
+      { path: "/react-test-deploy/", element: <Flashcards /> },
+      { path: "/react-test-deploy/", element: <Games /> },
+      { path: "/react-test-deploy/", element: <Quizzes /> },
     ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <>
-    <head>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
-        integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e"
-        crossorigin="anonymous"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet"
-      />
-      <link rel="icon" href="./img/networking.svg" />
-      <title>Nettie</title>
-    </head>
     <RouterProvider router={router} />
   </>
 );
