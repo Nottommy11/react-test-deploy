a1eed52c7ca6a44162df0d9ee7c01358644ccb10
Nottommy11 <thomas.marxsen11@gmail.com>
2023-02-04 15:42:56 -0600

    Navbar complete and routing working

---
+++ src/components/navbar.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import iconImg from "../img/networking.svg";
+import "./navbar.css";
+
+export default function Navbar() {
+  const [allClick, setAllClick] = useState(false);
+
+  const handleAllClick = () => setAllClick(!allClick);
+
+  const closeAllMobileMenu = () => setAllClick(false);
+
+  //For About and Contact
+  const [infoClick, setInfoClick] = useState(false);
+
+  const handleInfoClick = () => setInfoClick(!infoClick);
+
+  const closeInfoMobileMenu = () => setInfoClick(false);
+
+  const screenWidth = window.addEventListener("resize", () => {
+    return window.innerWidth;
+  });
+
+  return (
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <nav className="top-nav">
+        <div className="nav-container">
+          <Link
+            to="/"
+            className="nav-logo"
+            onClick={({ closeAllMobileMenu }, { closeInfoMobileMenu })}
+          >
+            <img className="nav-img" src={iconImg} alt="Nettie Icon" />
+            Nettie
+          </Link>
+          <ul className="all-menu-icon" onClick={handleAllClick}>
+            {allClick ? <FaTimes /> : <FaBars />}
+          </ul>
+          <ul className={allClick ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-learn">
+              <li className="nav-item">
+                <NavLink
+                  to="/flashcards"
+                  className={({ isActive }) =>
+                    "nav-links" + (isActive ? " activated" : "")
+                  }
+                  onClick={({ closeAllMobileMenu }, { closeInfoMobileMenu })}
+                >
+                  Flashcards
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/games"
+                  className={({ isActive }) =>
+                    "nav-links" + (isActive ? " activated" : "")
+                  }
+                  onClick={({ closeAllMobileMenu }, { closeInfoMobileMenu })}
+                >
+                  Games
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/quizzes"
+                  className={({ isActive }) =>
+                    "nav-links" + (isActive ? " activated" : "")
+                  }
+                  onClick={({ closeAllMobileMenu }, { closeInfoMobileMenu })}
+                >
+                  Quizzes
+                </NavLink>
+              </li>
+            </ul>
+            <div className="info-menu-icon" onClick={handleInfoClick}>
+              {infoClick ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul
+              className={infoClick ? "nav-info-menu active" : "nav-info-menu"}
+            >
+              <li className="nav-item nav-info-item">
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    "nav-links nav-info-links" + (isActive ? " activated" : "")
+                  }
+                  onClick={({ closeAllMobileMenu }, { closeInfoMobileMenu })}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item nav-info-item">
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    "nav-links nav-info-links" + (isActive ? " activated" : "")
+                  }
+                  onClick={({ closeAllMobileMenu }, { closeInfoMobileMenu })}
+                  onResize={{}}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </ul>
+        </div>
+      </nav>
+    </IconContext.Provider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Navbar from "./components/navbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Home from "./routes/home";
+import About from "./routes/about";
+import Contact from "./routes/contact";
+import Flashcards from "./routes/flashcards";
+import Games from "./routes/games";
+import Quizzes from "./routes/quizzes";
+import ErrorPage from "./routes/error";
+
+import "./index.css";
+
+const AppLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/flashcards", element: <Flashcards /> },
+      { path: "/games", element: <Games /> },
+      { path: "/quizzes", element: <Quizzes /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <head>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
+        integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e"
+        crossorigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="icon" href="./img/networking.svg" />
+      <title>Nettie</title>
+    </head>
+    <RouterProvider router={router} />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
